32ae39f55ae7cb2dcd2ae5ef55203f74be97fd30
jayp822 <jaydpat0822@gmail.com>
2024-02-11 07:49:08 -0500

    complete(beta)

--- src/frontend/MapDisplay/MapDisplay.js
+++ src/frontend/MapDisplay/MapDisplay.js
@@ -1,121 +1,448 @@
-"use client";
-import { BACKEND_URL } from "../constants.js";
-import { useState, useEffect } from "react";
-import useSWR from "swr";
+/** @format */
+
+import React, { Fragment } from "react";
+import {
+	GoogleMap,
+	LoadScript,
+	MarkerF,
+	InfoWindowF,
+	CircleF
+} from "@react-google-maps/api";
+import { useEffect, useState } from "react";
 import "./map.css";
 import kid from "./african_orphan_child_400x400.jpg";
-import usePlacesAutocomplete, {
-	getGeocode,
-	getLatLng
-} from "use-places-autocomplete";
-import {
-	APIProvider,
-	Map,
-	AdvancedMarker,
-	Pin,
-	InfoWindow
-} from "@vis.gl/react-google-maps";
+import { Link } from "react-router-dom";
+import { BACKEND_URL, withRouter } from "../constants.js";
 
-import {
-	Box,
-	Button,
-	ButtonGroup,
-	Flex,
-	HStack,
-	IconButton,
-	Input,
-	SkeletonText,
-	Text
-} from "@chakra-ui/react";
-import { FaLocationArrow, FaTimes } from "react-icons/fa";
+// import { Loader } from "@googlemaps/js-api-loader";
 
-import {
-	useJsApiLoader,
-	GoogleMap,
-	Marker,
-	Autocomplete,
-	DirectionsRenderer
-} from "@react-google-maps/api";
+// const loader = new Loader({
+//   apiKey: "AIzaSyBGNeut_skfFi5jKeAEzy3zrRp45RvcNf4"
+// })
 
+// let google;
 
-	import { Link } from "react-router-dom";
+//  loader.load().then((googl) => {
+//   console.log(googl);
+//   google = googl;
+// })
 
+const sad_kids = [
+	{
+		img_url:
+			"https://www.calmkidcentral.com/wp-content/uploads/2017/07/iStock-485139708.jpg"
+	},
+	{
+		img_url:
+			"https://www.calmkidcentral.com/wp-content/uploads/2017/07/iStock-485139708.jpg"
+	},
+	{
+		img_url:
+			"https://www.calmkidcentral.com/wp-content/uploads/2017/07/iStock-485139708.jpg"
+	},
+	{
+		img_url:
+			"https://www.calmkidcentral.com/wp-content/uploads/2017/07/iStock-485139708.jpg"
+	},
+	{
+		img_url:
+			"https://www.calmkidcentral.com/wp-content/uploads/2017/07/iStock-485139708.jpg"
+	},
+	{
+		img_url:
+			"https://www.calmkidcentral.com/wp-content/uploads/2017/07/iStock-485139708.jpg"
+	}
+];
 
-export default function Intro({ userType }) {
-	const position = { lat: 53.54, lng: 10 };
-	const [open, setOpen] = useState(false);
+class GoogleMapShit extends React.Component {
+	constructor(props) {
+		super(props);
 
-	return (
-		<div>
+		this.defaultCoords = {
+			lat: 33.957409,
+			lng: -83.376801
+		};
+		this.currentPosition = {};
+		this.selected = {};
+
+		this.places = [];
+		this.state = {
+			distance: 50,
+			age: 7,
+			type: []
+		};
+
+		// const places = [
+		// 	{
+		// 		name: "Location 1",
+		// 		location: {
+		// 			lat: 33.9519,
+		// 			lng: 83.3576
+		// 		},
+		// 		img_url:
+		// 			"https://www.calmkidcentral.com/wp-content/uploads/2017/07/iStock-485139708.jpg"
+		// 	},
+		// 	{
+		// 		name: "Location 2",
+		// 		location: {
+		// 			lat: 41.3917,
+		// 			lng: 2.1649
+		// 		},
+		// 		img_url:
+		// 			"https://www.calmkidcentral.com/wp-content/uploads/2017/07/iStock-485139708.jpg"
+		// 	},
+		// 	{
+		// 		name: "Location 3",
+		// 		location: {
+		// 			lat: 41.3773,
+		// 			lng: 2.1585
+		// 		},
+		// 		img_url:
+		// 			"https://www.calmkidcentral.com/wp-content/uploads/2017/07/iStock-485139708.jpg",
+		// 		circle: {
+		// 			radius: 1000,
+		// 			strokeColor: "#ff0000"
+		// 		}
+		// 	},
+		// 	{
+		// 		name: "Location 4",
+		// 		location: {
+		// 			lat: 41.3797,
+		// 			lng: 2.1682
+		// 		},
+		// 		img_url:
+		// 			"https://www.calmkidcentral.com/wp-content/uploads/2017/07/iStock-485139708.jpg",
+		// 		circle: {
+		// 			radius: 1000,
+		// 			strokeColor: "#ff0000"
+		// 		}
+		// 	},
+		// 	{
+		// 		name: "Location 5",
+		// 		location: {
+		// 			lat: 41.4055,
+		// 			lng: 2.1915
+		// 		},
+		// 		img_url:
+		// 			"https://www.calmkidcentral.com/wp-content/uploads/2017/07/iStock-485139708.jpg"
+		// 	}
+		// ];
+		this.fetchPlacesData().then(places => this.updatePlaces(places));
+		// this.updatePlaces();
+	}
+
+	getPos = async () => {
+		let { lat, lng } = await new Promise((res, rej) => {
+			navigator.geolocation.getCurrentPosition(pos => {
+				res({ lat: pos.coords.latitude, lng: pos.coords.longitude });
+			});
+			setTimeout(res, 1000, { ...this.defaultCoords });
+		});
+		return { lat, lng };
+	};
+
+	fetchPlacesData = async () => {
+		const { token } = this.props;
+		let { distance, age, type } = this.state;
+
+		const { lat, lng } = await this.getPos();
+
+		if (type.length === 2) type = [];
+		if (type.length > 0) {
+			type = type[0] === "type1" ? "POLICE" : "CIVIL";
+		}
+
+		if (token == null) {
+			return;
+		}
+
+		let url = `${BACKEND_URL}/api/get_reports?lat=${lat}&lng=${lng}&distance=${distance}&age=${age}`;
+
+		if (type.length > 0) {
+			url += `&type=${type}`;
+		}
+
+		const resp = await fetch(url, {
+			method: "GET",
+			headers: {
+				authorization: token
+			}
+		});
+
+		const data = await resp.json();
+		if (resp.status === 200) {
+			const places = data.reports;
+			console.log(places);
+			if (places && places.length > 0) {
+				places.forEach((place, i) => {
+					if (place.lat && place.lng)
+						place.location = {
+							lat: place.lat,
+							lng: place.lng
+						};
+
+					if (place.radius) {
+						const color = place.type === "POLICE" ? "#0000ff" : "#ff0000";
+						place.circle = {
+							radius: place.radius,
+							strokeColor: color
+						};
+					}
+
+					place.img_url = sad_kids[i % sad_kids.length].img_url;
+				});
+				return places;
+			}
+		} else {
+			alert(`Error! ${data.error}`);
+		}
+	};
+
+	setCurrentPosition = position => {
+		this.currentPosition = {
+			lat: position.lat,
+			lng: position.lng
+		};
+		this.forceUpdate();
+	};
+
+	setSelectedItem = item => {
+		this.selected = item;
+		console.log('selected', item)
+		this.forceUpdate();
+	};
+
+	onInfoWindowClick = (e, item) => {
+		if (e.target === item)
+			this.props.router.navigate("/report-information", {
+				reportId: e.targetReportId
+			});
+	};
+
+	onMarkerDragEnd = event => {
+		const lat = event.latLng.lat();
+		const lng = event.latLng.lng();
+		this.setCurrentPosition({ lat, lng });
+		this.forceUpdate();
+	};
+
+	/**
+	 * @type {
+	 *  (places: {
+	 *    location: {
+	 *      lat : number;
+	 *      lng: number;
+	 *    };
+	 *    circle: {
+	 *      radius: number,
+	 *      options: {
+	 *        strokeColor: number
+	 *      }
+	 *    };
+	 *    label: {
+	 *      name: string;
+	 *      image: string;
+	 *      targetReportId: string
+	 *
+	 *    }
+	 *  }
+	 *  ) => bool } places
+	 */
+	updatePlaces = places => {
+		this.places = [];
+		let i = 0;
+		for (const place of places) {
+			place.iid = i++;
+			this.places.push(place);
+		}
+
+		this.forceUpdate();
+	};
+
+	onDistanceChange = event => {
+		this.setState({ distance: event.target.value * 1000 }); // kilometers
+	};
+
+	onAgeChange = event => {
+		this.setState({ age: event.target.value });
+	};
+
+	onTypeChange = event => {
+		const { name, checked } = event.target;
+		const { type } = this.state;
+
+		let updatedTypes;
+		if (checked) {
+			updatedTypes = [...type, name];
+		} else {
+			updatedTypes = type.filter(t => t !== name);
+		}
+
+		console.log("updated", updatedTypes);
+		this.setState({ type: updatedTypes });
+	};
+
+	onSubmit = async () => {
+		let places = await this.fetchPlacesData();
+
+		this.updatePlaces(places);
+	};
+
+	switch = async (item) => {
+		this.setSelectedItem({});
+		console.log('switching to item', item)
+		this.props.router.navigate("/report-information", {
+			state: {
+				id: item.id
+			}
+		})
+	}
+
+	getMap() {
+		return (
+			<GoogleMap
+				mapContainerClassName="map-canvas"
+				center={this.defaultCoords}
+				zoom={13}>
+				{this.places.map(item => {
+					return (
+						item.location && (
+							<Fragment key={item.iid}>
+								{/* <MarkerF
+											key={item.name}
+											position={item.location}
+											onClick={() => this.setSelectedItem(item)}
+											onDragEnd={e => this.onMarkerDragEnd(e)}
+											draggable={false}
+										/> */}
+
+								<MarkerF
+									key={item.name}
+									position={item.location}
+									onClick={() => this.setSelectedItem(item)}
+									onDragEnd={e => this.onMarkerDragEnd(e)}
+									draggable={false}>
+									{/* <InfoWindowF
+												position={item.location}
+												clickable={true}
+												onCloseClick={() => this.setSelectedItem({})}>
+												<div>
+													<img
+														src={item.img_url}
+														className="kid_pic"
+														// style={{ width: "100px" }}
+														alt="sad_girl"
+													/>
+												</div>
+											</InfoWindowF> */}
+								</MarkerF>
+
+								{item.circle && (
+									<CircleF
+										center={item.location}
+										radius={item.circle.radius}
+										options={{
+											fillColor: item.circle.strokeColor,
+											strokeColor: item.circle.strokeColor
+										}}
+
+										// options={item.circle.options}
+									/>
+								)}
+								{this.selected.location && (
+									<InfoWindowF
+										position={this.selected.location}
+										clickable={true}
+										onCloseClick={() => this.setSelectedItem({})}>
+										<p className="kid-name" onClick={() => this.switch(this.selected)}>
+											hi
+											<img
+												src={item.img_url}
+												className="kid_pic"
+												alt="sad_girl"
+											/>
+										</p>
+									</InfoWindowF>
+								)}
+							</Fragment>
+						)
+					);
+				})}
+			</GoogleMap>
+		);
+	}
+
+	render() {
+		const { distance, age, type } = this.props;
+
+		return (
 			<div>
-				<ul>
-					<Link to="/report">Report</Link>
-				</ul>
-			</div>
-			<div>
-				<ul>
-					{userType === "map" ? (
-						<Link to="/">Main Page</Link>
-					) : (
-						<Link to="/signin">Main Page</Link>
-					)}
-				</ul>
-			</div>
-
-			<APIProvider apiKey="AIzaSyBGNeut_skfFi5jKeAEzy3zrRp45RvcNf4" region="">
-				<div id="container">
-					<Map
-						zoom={9}
-						center={position}
-						mapId="5743bb516df58986"
-						className="map-canvas">
-						<AdvancedMarker position={position} onClick={() => setOpen(true)}>
-							<Pin
-								background={"grey"}
-								borderColor={"green"}
-								glyphColor={"purple"}
+				<h1 id="mapVisualize">Map Visualization</h1>
+				<div className="bigBox">
+					<div>
+						<div className="input-card">
+							<label htmlFor="distance">Distance(Kilometers)</label>
+							<input
+								type="text"
+								name="distance"
+								id="distance"
+								onChange={this.onDistanceChange}
 							/>
-						</AdvancedMarker>
-
-						{open && (
-							<InfoWindow
-								position={position}
-								onCloseClick={() => setOpen(false)}>
-								<p>I'm in Hamburg</p>
-							</InfoWindow>
-						)}
-					</Map>
+						</div>
+						<div className="input-card">
+							<label htmlFor="age">Range of Date from Now</label>
+							<input
+								type="text"
+								name="age"
+								id="age"
+								onChange={this.onAgeChange}
+							/>
+						</div>
+						<div className="checkbox-row">
+							<label for="type1">Police</label>
+							<input
+								type="checkbox"
+								id="type1"
+								name="type1"
+								value="Police"
+								onChange={this.onTypeChange}
+							/>
+							<label for="type2">Civilian</label>
+							<input
+								type="checkbox"
+								id="type2"
+								name="type2"
+								value="Civilian"
+								onChange={this.onTypeChange}
+							/>
+						</div>
+						<button type="submit" value="submit" onClick={this.onSubmit}>
+							Submit
+						</button>
+					</div>
+					<div className="navigation">
+						<div>
+							<Link to="/report">
+								<button>New Report</button>
+							</Link>
+						</div>
+						<div>
+							<Link to="/report-information">
+								<button>View Report</button>
+							</Link>
+						</div>
+						<div>
+							<Link to="/">
+								<button>Return Home</button>
+							</Link>
+						</div>
+					</div>
 				</div>
-			</APIProvider>
-		</div>
-	);
+				<div className="googleMap">{this.getMap()}</div>
+			</div>
+		);
+	}
 }
 
-
-// export default function Intro({ userType })
-// {
-//  /**
-//   * @type {(distance: number, age: number, type: 'civil' | 'police') => any[]} test
-//   */
-//  const fetchReportData = async (dist, age, type) =>
-//  {
-//   const token = this.props.token;
-
-//   const url = `${BACKEND_URL}/api/get_reports/?distance=${dist}&age=${age}&type=${type}`;
-//   const resp = await fetch(url, { headers: { authorization: token } });
-
-//   if (resp.status !== 200)
-//   {
-//    console.error("invalid database query");
-//    return [];
-//   }
-
-//   const { reports } = await resp.json();
-
-//   return reports;
-//  };
-// }
-
-
-
-
+const _test = withRouter(GoogleMapShit);
+export default _test;
